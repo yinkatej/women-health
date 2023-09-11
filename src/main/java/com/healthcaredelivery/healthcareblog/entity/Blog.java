0384bcfa68d42f80ae11d7656ac336d7bf806722
yinkatej <yinkatej@gmail.com>
2023-09-11 16:55:40 +0100

    cascade

--- src/main/java/com/healthcaredelivery/healthcareblog/entity/Blog.java
+++ src/main/java/com/healthcaredelivery/healthcareblog/entity/Blog.java
@@ -17,7 +17,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class Blog {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     // @Column(unique = true)
     private String title;
